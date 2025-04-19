620372560d1d707fc2b552c9fa34cfa7b689344d
Krizo <kristof-gabor.balinth@telekom.com>
2025-04-19 18:19:58 +0200

    init + pages

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<NuxtRouteAnnouncer />
+		<NuxtLayout>
+			<!-- Header Start -->
+			<header>
+				<ul>
+					<li>
+						<nuxt-link to="/">Home</nuxt-link>
+						<nuxt-link to="/events">Events</nuxt-link>
+					</li>
+				</ul>
+			</header>
+			<!-- Header Ends -->
+
+			<NuxtPage />
+		</NuxtLayout>
+	</div>
+</template>
---
+++ pages/events/index.vue
@@ -0,0 +1,5 @@
+<template>
+	<div>
+		<p>events page</p>
+	</div>
+</template>
